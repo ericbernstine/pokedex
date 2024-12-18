2e02d78f8e49f99901426cf26557f3e2138a1eb7
Eric Spencer Bernstine <ericbernstine@Erics-MBP.ht.home>
2024-12-18 15:26:21 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    <header>
+      <h1>Pokedex App</h1>
+    </header>
+
+    <div className='container'>
+      <div className='pokemon'>
+        
+      </div>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
